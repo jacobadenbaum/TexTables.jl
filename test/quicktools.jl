da1754cf14557c8acfc1b43cb0071f48b1c9d778
Jacob Adenbaum <jacobadenbaum@gmail.com>
2018-06-29 18:45:26 -0500

    Forgot to add new test resources, causing build to fail on travis

---
+++ test/quicktools.jl
@@ -0,0 +1,16 @@
+
+using   RDatasets
+import   TexTables: tuplefy
+
+@testset "tuplefy" begin
+    vals = [1, 1.0, "1", :1]
+    for x in vals
+        @test tuplefy(x) == (x,)
+    end
+    for x in [(val,) for val in vals]
+        @test tuplefy(x) == x
+    end
+    for x in vals, for y in vals
+        @test tuplefy((x, y)) == (x,y)
+    end
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ using   Base.Test
 include("helper.jl")
 
 tests = [   "tablecol", "composite_tables", "indexing", "printing",
-            "examples"]
+            "examples", "quicktools"]
 
 @testset "TexTables" begin
     for testsuite in tests
